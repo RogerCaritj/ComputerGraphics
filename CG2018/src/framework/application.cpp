fe061ed78aef923861ff6c1aa58cb8099f772eb5
RogerCaritj <43952278+RogerCaritj@users.noreply.github.com>
2019-01-12 20:38:30 +0100

    v.0.1

--- CG2018/src/framework/application.cpp
+++ CG2018/src/framework/application.cpp
@@ -30,21 +30,35 @@ void Application::init(void)
 }
 void bigerPixel(Image* img)
 {
-	//for (int i = 0; i<10; i++)
-	//{
-	//	img->setpixel(10 + i, 500, color::white);
-	//}
 	int randomColor1 = (((frand()) * 255) % 255);
 	int randomColor2 = (((frand()) * 255) % 255);
 	int randomColor3 = (((frand()) * 255) % 255);
-	for(int x = 400; x<440; x++)
-		for (int y = 300; y < 340; y++) {
+	for(int x = 380; x<420; x++)
+		for (int y = 280; y < 320; y++) {
 			img->setPixelSafe(x, y, Color(randomColor1, randomColor2, randomColor3)); //random color
 		}
-	//img->setPixelSafe(400, 300, Color(255, 255, 255)); //random color
-	//img->setPixelSafe(401, 300, Color(255, 255, 255)); //random color
-	//img->setPixelSafe(400, 301, Color(255, 255, 255)); //random color
-	//img->setPixelSafe(401, 301, Color(255, 255, 255)); //random color
+}
+
+void drawForms(Image* img) 
+{
+	int randomColor1 = (((frand()) * 255) % 255);
+	int randomColor2 = (((frand()) * 255) % 255);
+	int randomColor3 = (((frand()) * 255) % 255);
+
+	//Right
+	
+		for (int y = 298; y < 302; y++) {
+			if ((420 + count) > 470) {
+				for (int x = 420; x < 470; x++) {
+					img->setPixelSafe(x, y, Color(255, 0, 0)); //random color
+				}
+			}
+			else {
+				img->setPixelSafe(420 + count, y, Color(255, 0, 0)); //random color
+			}
+
+		}
+	count = count + 0.1;	
 }
 
 void dividePixel(Image* img) {
@@ -54,23 +68,34 @@ void dividePixel(Image* img) {
 	
 
 		//Down left
-		for (int x = 400; x < 420; x++)
-			for (int y = 300; y < 320; y++) {
-				img->setPixelSafe(x - count * 40, y, Color(randomColor1, randomColor2, randomColor3)); //random color
+		for (int x = 380; x < 400; x++)
+			for (int y = 280; y < 300; y++) {
+				if (changeRoute == 1) {
+					for (int z = 180; z < 201; z++) {
+						img->setPixelSafe(z + 20 + 40 * -sin(count + 1.45), y + 100*cos(3*count), Color(randomColor1, randomColor2, randomColor3)); //random color
+					}
+				}
+				else {
+					img->setPixelSafe(x - count * 40, y, Color(randomColor1, randomColor2, randomColor3)); //random color
+				}
 			}
 		//Down right
-		for (int x = 420; x < 440; x++)
-			for (int y = 300; y < 320; y++) {
-				img->setPixelSafe(x, y - count * 40, Color(randomColor1, randomColor2, randomColor3)); //random color
+		for (int x = 400; x < 420; x++)
+			for (int y = 280; y < 300; y++) {
+				if (changeRoute == 1) {
+					for (int z = 80; z < 101; z++) {
+						img->setPixelSafe(x, z + 20 + 40 * -sin(count + 1.45), Color(randomColor1, randomColor2, randomColor3)); //random color
+					}
+				}
+				else {
+					img->setPixelSafe(x, y - count * 40, Color(randomColor1, randomColor2, randomColor3)); //random color
+				}
 			}
 		//Up left
-		for (int x = 400; x < 420; x++)
-			for (int y = 320; y < 340; y++) {
-				if (y + count * 40 > 520) {
-					changeRoute = 1;
-				}
+		for (int x = 380; x < 400; x++)
+			for (int y = 300; y < 320; y++) {
 				if (changeRoute == 1) {
-					for (int z = 500; z < 521; z++) {
+					for (int z = 480; z < 501; z++) {
 						img->setPixelSafe(x, z + 20 + 40 * sin(count + 1.45), Color(randomColor1, randomColor2, randomColor3)); //random color
 					}
 				}
@@ -79,21 +104,21 @@ void dividePixel(Image* img) {
 				}
 			}
 		//Up right
-		for (int x = 420; x < 440; x++)
-			for (int y = 320; y < 340; y++) {
-				if (x + count * 40 > 620) {
+		for (int x = 400; x < 420; x++)
+			for (int y = 300; y < 320; y++) {
+				if (x + count * 40 > 600) {
 					changeRoute = 1;
 				}
 				if (changeRoute == 1) {
-					for (int z = 600; z < 621; z++) {
-						img->setPixelSafe(z + 20 + 40 * sin(count + 1.45), y, Color(randomColor1, randomColor2, randomColor3)); //random color
+					for (int z = 580; z < 601; z++) {
+						img->setPixelSafe(z + 20 + 40 * sin(count + 1.45), y + 100*-cos(3 * count), Color(randomColor1, randomColor2, randomColor3)); //random color
 					}
 				}
 				else {
 					img->setPixelSafe(x + count * 40, y, Color(randomColor1, randomColor2, randomColor3)); //random color
 				}
 			}
-		count = count + 0.01;
+		count = count + 0.05;
 }
 
 //render one frame
@@ -109,13 +134,11 @@ void Application::render(void)
 		showImage(&framebuffer);
 	}
 	else if (modo == 2) {
-		framebuffer.setPixelSafe(200 * time, 200, Color(0, 255, 0)); //random color
-		framebuffer.setPixelSafe(201 * time, 200, Color(0, 255, 0)); //random color
-		framebuffer.setPixelSafe(200 * time, 201, Color(0, 255, 0)); //random color
-		framebuffer.setPixelSafe(201 * time, 201, Color(0, 255, 0)); //random color
+		bigerPixel(&framebuffer);
+		drawForms(&framebuffer);
 		showImage(&framebuffer);
 	}
-	else {
+	else{
 
 		bigerPixel(&framebuffer);
 		showImage(&framebuffer);
@@ -133,14 +156,20 @@ void Application::update(double seconds_elapsed)
 	//to see all the keycodes: https://wiki.libsdl.org/SDL_Keycode
 	if (keystate[SDL_SCANCODE_SPACE]) //if key space is pressed
 	{
+		count = 0;
+		changeRoute = 0;
+		modo = 0;
 		
 	}
 	if (keystate[SDL_SCANCODE_1]) //if key 1 is pressed
 	{
+		count = 0;
+		changeRoute = 0;
 		modo = 1;
 	}
 	if (keystate[SDL_SCANCODE_2]) //if key 2 is pressed
 	{
+		count = 0;
 		modo = 2;
 	}
 
